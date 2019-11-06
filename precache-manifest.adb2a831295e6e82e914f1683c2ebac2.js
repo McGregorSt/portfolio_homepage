456ceb75c31ef9b6a8b8d11a9d8d189e7fc114e6
McGregorSt <stasiak.gregor@gmail.com>
2019-11-06 11:34:01 +0100

    Updates

---
+++ precache-manifest.adb2a831295e6e82e914f1683c2ebac2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b156dab99f285ce91f3b14d2b4558c5",
+    "url": "/portfolio_homepage/index.html"
+  },
+  {
+    "revision": "1f2530164965cc2c1b51",
+    "url": "/portfolio_homepage/static/css/main.24d63fba.chunk.css"
+  },
+  {
+    "revision": "587a900c9cafb6fe1560",
+    "url": "/portfolio_homepage/static/js/2.ce83b6c4.chunk.js"
+  },
+  {
+    "revision": "1f2530164965cc2c1b51",
+    "url": "/portfolio_homepage/static/js/main.8d9e020c.chunk.js"
+  },
+  {
+    "revision": "65b6e063768a0c7b5f17",
+    "url": "/portfolio_homepage/static/js/runtime-main.b0410ba0.js"
+  },
+  {
+    "revision": "9c5b12ad8898c050c6674a6cc0a24fc6",
+    "url": "/portfolio_homepage/static/media/marvin.9c5b12ad.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/homepage/precache-manifest.8738534a29684bed6b6efff24f96f2c3.js"
+  "/portfolio_homepage/precache-manifest.adb2a831295e6e82e914f1683c2ebac2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio_homepage/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
