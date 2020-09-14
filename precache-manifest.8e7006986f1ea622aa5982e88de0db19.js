be8bd0aac01a22aa70e9ac1756a983acd8ff0ce5
Grzegorz Stasiak <stasiak.gregor@gmail.com>
2020-09-14 21:50:58 +0200

    Updates

--- precache-manifest.8e7006986f1ea622aa5982e88de0db19.js
+++ precache-manifest.8e7006986f1ea622aa5982e88de0db19.js
@@ -1,19 +1,19 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "b14566ea5f4e804b5260a2626687dc32",
+    "revision": "0f73f53bdee947963563699d0474a6b0",
     "url": "/portfolio/index.html"
   },
   {
-    "revision": "f4760e82d7b69a8dfa0c",
-    "url": "/portfolio/static/css/main.78db937e.chunk.css"
+    "revision": "728a400c576bc39d1abf",
+    "url": "/portfolio/static/css/main.dbcd2180.chunk.css"
   },
   {
     "revision": "84203ff2926a81955f2f",
     "url": "/portfolio/static/js/2.9b3b7a23.chunk.js"
   },
   {
-    "revision": "f4760e82d7b69a8dfa0c",
-    "url": "/portfolio/static/js/main.b21eef3a.chunk.js"
+    "revision": "728a400c576bc39d1abf",
+    "url": "/portfolio/static/js/main.a9b7fe4f.chunk.js"
   },
   {
     "revision": "4a341bc0cf8cd62c0688",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.9237bee9d2dc71bde2eccb32f18012a8.js"
+  "/portfolio/precache-manifest.8e7006986f1ea622aa5982e88de0db19.js"
 );
 
 self.addEventListener('message', (event) => {
